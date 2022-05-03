315b9d15e482c26e76ab2d5967ed0e83378add8f
SR.Chen <sr.0927.chen@gmail.com>
2022-05-03 10:05:00 +0800

    updata

--- js/script.js
+++ js/script.js
@@ -45,13 +45,38 @@ function getScrollTop(){
     }
 }
 
+var url = "https://script.google.com/macros/s/AKfycbzlqYeyZUTyt8KxaI2oZ_gnjfrX11XXT5Y0mz3xsjdCliZRPVvMbvuRxZ6vRFZKgkCM/exec"
+var listurl = "https://docs.google.com/spreadsheets/d/1Y1kHci7Mi_lIQy4IkP9PX1OvRIkzM1xHPVu-9-Oiswk/edit?usp=sharing"
 
 carousel_item = document.querySelectorAll(".carousel-item");
 navbar = document.querySelector("nav");
-
+form_btn = document.querySelector("#form-btn");
 item_list = Array.from(carousel_item);
 carousel();
 
 window.addEventListener("scroll",(e)=>{
     getScrollTop()
+})
+
+form_btn.addEventListener("click",(e)=>{
+    e.preventDefault();
+    let name = document.querySelector("#form-name").value;
+    let email = document.querySelector("#form-email").value;
+    let main = document.querySelector("#form-main").value;
+    let message = document.querySelector("#form-message").value;
+    var date = new Date();
+    $.ajax({
+        url: "https://script.google.com/macros/s/AKfycbx8-S3LBMFGeGzF5TFdxL2DNYJ03r_KKvfabPjKapyYnEcZTTs3kPNXFabRormAjRaT/exec",
+        data: {
+            "date": date,
+            "name": name,
+            "email": email,
+            "main": main,
+            "message": message
+        },
+        success: function(response) {
+          if(response == "成功"){
+          }
+        },
+    });
 })
